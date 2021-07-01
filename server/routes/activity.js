a345fed8697e5e1f1a19c707c4361ec2a09ea2fa
Jenny Svensson <hello@jennysvensson.com>
2021-07-01 18:12:19 +0200

    Added categories on server

--- server/routes/activity.js
+++ server/routes/activity.js
@@ -2,12 +2,24 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 const fs = require('fs');
+const addToFile = require('../service/addToFile');
 
 const activitiesFilePath = path.join(__dirname, '../db/activities.json');
+const categoriesFilePath = path.join(__dirname, '../db/categories.json');
 
 router.get('/', function (req, res, next) {
   const readable = fs.createReadStream(activitiesFilePath);
   readable.pipe(res);
+});
+
+router.get('/category/', (req, res, next) => {
+  const readable = fs.createReadStream(categoriesFilePath);
+  readable.pipe(res);
+});
+
+router.post('/', (req, res, next) => {
+  addToFile(activitiesFilePath, req.body);
+  res.status(201).end();
 });
 
 module.exports = router;
--- server/service/addToFile.js
+++ server/service/addToFile.js
@@ -2,15 +2,19 @@ const fs = require('fs');
 
 const addToFile = (file, data) => {
   const currentFileData = fs.readFileSync(file);
-  const currentFileJson = JSON.parse(currentFileData);
-  const newData = data;
-  currentFileJson.push(newData);
-  const newFileData = JSON.stringify(currentFileJson);
-  fs.writeFile(file, newFileData, (err) => {
-    if (err) {
-      throw createError(500, 'Error writing to file');
-    }
-  });
+  try {
+    const currentFileJson = JSON.parse(currentFileData);
+    const newData = data;
+    currentFileJson.push(newData);
+    const newFileData = JSON.stringify(currentFileJson, null, "\t");
+    fs.writeFile(file, newFileData, (err) => {
+      if (err) {
+        throw createError(500, 'Error writing to file');
+      }
+    });
+  } catch {
+    throw createError(400, 'Bad json');
+  }
 }
 
 module.exports = addToFile;
