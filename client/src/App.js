756893a44c9bfcb5ed4cf8481d1f8b0cad430ca4
Jenny Svensson <hello@jennysvensson.com>
2021-07-01 09:11:22 +0200

    Init client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+const App = () => {
+
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
